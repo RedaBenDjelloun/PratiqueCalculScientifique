82a794c91d62c6db3224751b63811c84a5cb76a5
hectormartinet <103176565+hectormartinet@users.noreply.github.com>
2022-05-30 23:05:08 +0200

    code bien nettoyé, tout beau tout propre

--- projet.jl
+++ projet.jl
@@ -6,59 +6,32 @@ pygui(true)
 
 #################### Paramètres ####################
 
+# user parameters
 p=2.0
 q=0.05
-d=0.1
-T=3 # durée de la simulation
+d=0.01
+T=10.0 # duration of the simulation
 Δx = 0.1
 Δt = 0.005
+
+# initial state u0 = u_eq + A * [sin(k1*x),cos(k2*x)]
 A = 1
-ω1 = 1
-ω2 = 2
+k1 = 1
+k2 = 2
+
+# computed parameters
 D =[1. 0.;0. d]
 nb_x = floor(Int,2*pi/Δx)
 nb_t = floor(Int,T/Δt)
 Δx = 2*pi/nb_x
 Δt = T/nb_t
-
-function norme(v)
-    return sum([elt^2 for elt in v])
+u0 = zeros(nb_x,2)
+for i in 1:nb_x
+    x = i*Δx
+    u0[i,:] = [p/((p+q)^2)+A*sin(k1*x),p+q+A*cos(k2*x)]
 end
 
-#################### Modélisation du problème ####################
-
-function f(u)
-    return [p-u[1]*u[2]^2;q-u[2]+u[1]*u[2]^2]
-end
-
-function ∇f(u)
-    return [-u[2]^2 -2*u[1]*u[2];u[2]^2 -1+2*u[1]*u[2]]
-end
-
-# applique f à tous les éléments du vecteur de vecteurs
-function F(u)
-    v = zeros(nb_x,2)
-    for i in 1:nb_x
-        v[i,:] = f(u[i,:])
-    end
-    return v
-end
-
-#################### Euler explicite ####################
-
-function ∇F(u)
-    v = zeros(2*nb_x,2*nb_x)
-    for i in 1:nb_x
-        df = ∇f([u[i];u[nb_x+i]])
-        v[i,i] = df[1,1]
-        v[i,nb_x+i] = df[1,2]
-        v[nb_x+i,i] = df[2,1]
-        v[nb_x+i,nb_x+i] = df[2,2]
-    end
-    return v
-end
-
-# M permet de faire des dérivées secondes discrètes
+# M is the discrete operator for second derivative
 M = zeros(nb_x,nb_x)
 for i in 1:nb_x-1
     M[i,i]=-2
@@ -68,18 +41,10 @@ end
 M[nb_x,nb_x]=-2
 M[nb_x,1]=1
 M[1,nb_x]=1
+M = M/Δx^2
 
-M2 = zeros(2*nb_x,2*nb_x)
-for j in [0,nb_x]
-    for i in 1:nb_x-1
-        M2[i+j,i+j]=-2
-        M2[i+j,i+j+1]=1
-        M2[i+j+1,i+j]=1
-    end
-    M2[nb_x+j,nb_x+j]=-2
-    M2[nb_x+j,1+j]=1
-    M2[1+j,nb_x+j]=1
-end
+# M2 is the discrete operator for 2nd derivative when u is in the vectorial form
+M2 = [M zeros(nb_x,nb_x); zeros(nb_x,nb_x) M]
 
 D2 = zeros(2*nb_x,2*nb_x)
 for i in 1:nb_x
@@ -87,109 +52,187 @@ for i in 1:nb_x
     D2[i+nb_x,i+nb_x]=d
 end
 
-# Etat initial = état stable + fluctuation
-function u0(x)
-    return  [p/((p+q)^2)+A*sin(ω1*x),p+q+A*cos(ω2*x)]
+
+###################### u representation ###################
+
+# reshape a matrix into a vector column by column
+function vect(u)
+    u[:]
+end
+
+#test de vect
+begin
+    x = [1 3;2 4]
+    @assert(vect(x)==[1,2,3,4])
+end
+
+# reshape a vector into a matrix with 2 columns
+# input [u1, u2, ... u2N]
+# output [u1 uN+1; u2 uN+2; ... ; uN u2N]
+function devect(u)
+    new_size = size(u)[1]÷2
+    devect_u = zeros(new_size,2)
+    for i in 1:new_size
+        devect_u[i,:] = [u[i], u[i+new_size]]
+    end
+    devect_u
+end
+
+# test de devect
+begin
+    x = [1,2,3,4]
+    @assert(devect(x)==[1 3; 2 4])
+end
+
+function deflatten(u)
+    v = zeros(size(u)[1]÷2,2,size(u)[2])
+    for i in 1:nb_x
+        v[i,1,:]=u[i,:,:]
+        v[i,2,:]=u[nb_x+i,:,:]
+    end
+    return v
+end
+
+#################### f and derivatives ####################
+
+f(u) = [p-u[1]*u[2]^2;q-u[2]+u[1]*u[2]^2]
+
+# Jacobienne de f
+Jf(u) =[-u[2]^2 -2*u[1]*u[2] ;
+         u[2]^2 -1+2*u[1]*u[2] ]
+
+# test de la Jacobienne
+begin
+    u = rand(2)
+    h = rand(2)
+    eps = 1e-8
+    @assert( norm( (f(u+eps*h)-f(u))/eps - Jf(u)*h ) < 1e-5 )
+end
+
+# input : u of the form u[i,c]
+# output : [f(u[i,:])]
+function F(u)
+    Fu = zero(u)
+    for i in 1:size(u)[1]
+        Fu[i,:] = f(u[i,:])
+    end
+    return Fu
+end
+
+# input : u of the form [u[:,1], u[:,2]]
+# output : [f(u[i,:])]
+function F2(u)
+    F2u = zero(u)
+    new_size = size(u)[1]÷2
+    for i in 1:new_size
+        fu = f([u[i],u[i+new_size]])
+        F2u[i] = fu[1]
+        F2u[new_size+i] =fu[2]
+    end
+    return F2u
+end
+
+#test
+begin
+    u = rand(50,2)
+    Fu1 = F(u)
+    Fu2 = devect(F2(vect(u)))
+    @assert(norm(Fu1-Fu2)<1e-10)
+end
+
+# input : u of the form [u[:,1], u[:,2]]
+# output : Jacobian of F at u
+function JF(u)
+    JFu = zeros(2*nb_x,2*nb_x)
+    for i in 1:nb_x
+        df = Jf([u[i];u[nb_x+i]])
+        JFu[i,i] = df[1,1]
+        JFu[i,nb_x+i] = df[1,2]
+        JFu[nb_x+i,i] = df[2,1]
+        JFu[nb_x+i,nb_x+i] = df[2,2]
+    end
+    return JFu
+end
+
+
+##################### Euler Explicite ######################
+
+function generic_explicit_euler(u0, f)
+    u = zeros(size(u0)[1],nb_t)
+
+    # initialization
+    u[:,1] = u0;
+
+    # integration with implicit euler method
+    for t in 1:(nb_t-1)
+        u[:,t+1] = u[:,t] + Δt*f(u[:,t])
+    end
+    return u
 end
 
 # Intégration de l'edp avec la méthode d'Euler explicite
-function euler_explicite(Δt,Δx,T, u0)
-    nb_x = floor(Int,2*pi/Δx)
-    Δx = 2*pi/nb_x # on arrondi Δx pour qu'il n'y ait pas de problème au bord
-    nb_t = floor(Int,T/Δt)
-    Δt = T/nb_t
-    u = zeros(nb_x,nb_t,2)
-
-    # initialisation
-    for k in 1:nb_x
-        u[k,1,:] = u0(k*Δx)
-    end
-
-    # intégration
-    for t in 1:(nb_t-1)
-        u[:,t+1,:] = u[:,t,:] + Δt*F(u[:,t,:]) + Δt/(Δx^2)*M*u[:,t,:]*D
-    end
-
-    return u
+function euler_explicite(u0)
+    u0 = vect(u0)
+    f(u) = D2*M2*u + F2(u)
+    sol = generic_explicit_euler(u0,f)
+    return deflatten(sol)
 end
 
 #################### Euler implicite ####################
 
-function g(Δt,Δx, x, u, t)
-    return x - u[:,t] - Δt*F2(x) - Δt/(Δx^2)*D2*M2*x
-end
-
-function ∇g(Δt,Δx,x)
-    return I-Δt*∇F(x)-Δt/(Δx^2)*D2*M2
-end
-
-# applique f à tous les éléments du vecteur de vecteurs
-function F2(u)
-    v = zeros(2*nb_x)
-    for i in 1:nb_x
-        x = f([u[i],u[i+nb_x]])
-        v[i] = x[1]
-        v[nb_x+i] =x[2]
-    end
-    return v
-end
-
-function flatten(u)
-    v = zeros(2*nb_x)
-    for  i in 1:nb_x
-        v[i,:]=u[i,:,1]
-        v[nb_x+i,:]=u[i,:,2]
-    end
-    return v
-end
-
-function deflatten(u)
-    v = zeros(nb_x,nb_t,2)
-    for i in 1:nb_x
-        v[i,:,1]=u[i,:,:]
-        v[i,:,2]=u[nb_x+i,:,:]
-    end
-    return v
-end
-
-function newton(g,∇g,Δt,Δx,x0,u,t,eps)
+function newton(f,Jf,x0,u_t,eps,max_iter)
     x=x0
     i=0
-    while(norme(g(Δt,Δx, x, u, t))>eps && i<100)
-        x = x - inv(∇g(Δt,Δx, x))*g(Δt,Δx, x, u, t)
+    while(norm(f(x)-u_t)>eps && i<max_iter)
+        x = x - inv(Jf(x))*(f(x)-u_t)
         i+=1
     end
     return x
 end
 
-function euler_implicite(Δt,Δx,T, u0)
-    nb_x = floor(Int,2*pi/Δx)
-    Δx = 2*pi/nb_x # on arrondi Δx pour qu'il n'y ait pas de problème au bord
-    nb_t = floor(Int,T/Δt)
-    Δt = T/nb_t
-    u = zeros(2*nb_x,nb_t)
+function generic_implicit_euler(u0,f,Jf)
+    u = zeros(size(u0)[1],nb_t)
 
-    # initialisation
-    for k in 1:nb_x
-        v = u0(k*Δx)
-        u[k,1] = v[1]
-        u[k+nb_x,1] = v[2]
-    end
+    # initialization
+    u[:,1] = u0;
 
-    # intégration
+    # integration
     for t in 1:(nb_t-1)
-        u[:,t+1] = newton(g,∇g,Δt,Δx, u[:,t], u, t,1e-5)
+        u[:,t+1] = newton(f,Jf, u[:,t], u[:,t],1e-5*nb_x,20)
     end
-
     return u
 end
 
+function euler_implicite(u0)
+    u0 = vect(u0)
+    f(x) = x  - Δt*F2(x) - Δt*D2*M2*x
+    Jf(x) = I-Δt*JF(x)-Δt*D2*M2
+
+    sol = generic_implicit_euler(u0,f,Jf)
+
+    return deflatten(sol)
+end
+
+#################### Boîte noire #######################
+function boite_noire(u0)
+    u0 = vect(u0)
+
+    # definition du problème
+    f(u,p,t) = F2(u) + D2*M2*u
+    tspan = (0.0,T)
+    prob = ODEProblem(f,u0,tspan)
+
+    # résolution
+    sol = solve(prob,saveat=Δt)
+
+    return deflatten(reduce(hcat,sol.u))
+end
+
+
 #################### Etude théorique ####################
 
-# Matrice d'eq diff pour les coeff de Fourrier
-function J(n,d)
-    return [-(p+q)^2 -2*p/(p+q); (p+q)^2 -1+2*p/(p+q)] - n^2*[1 0; 0 d]
-end
+# ̇cn = J(n,d)*cn avec cn le n-ième coefficient de Fourrier de u
+J(n,d) = [-(p+q)^2 -2*p/(p+q); (p+q)^2 -1+2*p/(p+q)] - n^2*[1 0; 0 d]
 
 # Affichage
 #X = [d for d in range(0,0.2,100)]
@@ -216,37 +259,9 @@ function dichotomie(eps)
     return (d_min+d_max)/2.0
 end
 
-#################### Animation ####################
+########################### Calcul de la solution #########################
+#u = euler_explicite(u0)
+#u = euler_implicite(u0)
+u = boite_noire(u0)
 
-tps_affichage = T
-fps = 25
-n=floor(Int,fps*tps_affichage)  # nombre de frames
-lst_t = [1/n*t*T for t in 1:n]  # liste des temps auxquels on affiche
-lst_i = [floor(Int,t/Δt) for t in lst_t]    # liste des indices correspondants
-
-# Calcul de la solution
-#u = euler_explicite(Δt,Δx,T,u0)
-u = euler_implicite(Δt,Δx,T,u0)
-u = deflatten(u)
-
-# abscisses pour
-X = [k*Δx for k in 1:nb_x]
-
-#Etats stables
-Y1 = ones(nb_x)*p/((p+q)^2)
-Y2 = ones(nb_x)*(p+q)
-
-# maximum et minimum de la solution pour l'affichage
-y1 = minimum(u)
-y2 = maximum(u)
-
-#animation
-anim = @animate for i ∈ 1:n
-    Plots.plot(X,[Y1 Y2 u[:,lst_i[i],1] u[:,lst_i[i],2]],legend=false,ylim=(y1,y2))
-end
-
-gif(anim, fps = fps)
-
-#Plots.plot(X,[Y1 Y2 u[:,nb_t,1] u[:,nb_t,2]],legend=false)
-
-####################  ####################
+heatmap(u[:,1,:])
